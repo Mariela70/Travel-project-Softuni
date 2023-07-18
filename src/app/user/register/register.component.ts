95f9f9e9c875e032625aa1182af055301979fe86
Mariela <marielamiteva97@abv.bg>
2023-07-18 23:01:44 +0300

    added register validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  constructor(private userService: UserService, private router: Router) { }
+  register(form: NgForm): void {
+    // this.userService.register();
+    this.router.navigate(['/']);
+  }
 }
