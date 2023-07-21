d03b6e380c2db981ef3ed82c2a728c462fd2d504
Mariela <marielamiteva97@abv.bg>
2023-07-21 23:49:52 +0300

    added logout component

---
+++ src/app/user/logout/logout.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+  constructor(private router: Router, private userService: UserService) {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.userService.user = undefined;
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.userService.user = undefined;
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
+}
