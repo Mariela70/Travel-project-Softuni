f4ae0bb38130e7ff4346291ff2563325a4b7ba77
Mariela <marielamiteva97@abv.bg>
2023-07-30 17:17:20 +0300

    added user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,35 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { UserId } from '../types/user-id';
+import { IUser } from '../shared/types/user-id';
+import { BehaviorSubject, Subscription, catchError, filter, of, tap } from 'rxjs';
 
+const { appUrl } = environment;
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-user: UserId | undefined;
+export class UserService implements OnDestroy {
 
-get isLogin() {
-  return this.user !== null;
-}
-  constructor(private http: HttpClient) { }
+  private user$$ = new BehaviorSubject<undefined | null | IUser>(undefined);
+  user$ = this.user$$.asObservable().pipe(filter((val): val is IUser | null => val !== undefined));
+  user: IUser | null = null;
 
-  register(firstName: string, lastName: string, email: string, password:string, rePassword:string) {
-    const {appUrl} = environment;
-    return this.http.post<UserId>(`${appUrl}/users/register`, {firstName, lastName, email, password, rePassword});
+  get isLoggedIn() {
+    return this.user !== null;
+  }
+  subscription: Subscription;
+  constructor(private http: HttpClient) {
+    this.subscription = this.user$.subscribe(user => {
+        this.user = user;
+      })
   }
 
-  login( email: string, password:string) {
-    const {appUrl} = environment;
-    return this.http.post<UserId>(`${appUrl}/users/login`, {email, password});
+  register(firstName: string, lastName: string, email: string, password: string, rePassword: string) {
+    return this.http.post<IUser>(`${appUrl}/users/register`, { firstName, lastName, email, password, rePassword })
+      .pipe(tap(user => this.user$$.next(user)));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<IUser>(`${appUrl}/users/login`, { email, password })
+      .pipe(tap(user => this.user$$.next(user)));
   }
   logout() {
-    const {appUrl} = environment;
     return this.http.post<void>(`${appUrl}/users/logout`, {})
+      .pipe(tap(() => this.user$$.next(null)));
   }
-  getMyProfile(){
-    const {appUrl} = environment;
-    return this.http.get<UserId>(`${appUrl}/data/destinations?where=_ownerId%3D%22${this.user}%22&sortBy=_createdOn%20desc`);
+  getMyProfile() {
+    return this.http.get<IUser>(`${appUrl}/data/profile`)
+      .pipe(tap(user => this.user$$.next(user)),
+      catchError((err) => {
+        this.user$$.next(null);
+        return of(err);
+      }));
+  }
+  // getMyProfile() {
+  //   return this.http.get<IUser>(`${appUrl}/data/destinations?where=_ownerId%3D%22${this.user}%22&sortBy=_createdOn%20desc`)
+  //     .pipe(tap(user => this.user$$.next(user)));
+  // }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
 
