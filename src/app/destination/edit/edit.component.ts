989f2a18695ddbaae7a44dc7d8739a3c8c5ce3c8
Mariela <marielamiteva97@abv.bg>
2023-08-03 18:01:28 +0300

    added edit functionality

--- src/app/destination/edit/edit.component.ts
+++ src/app/destination/edit/edit.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserService } from 'src/app/user/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DestinationService } from '../destination.service';
 
 @Component({
   selector: 'app-edit',
@@ -9,9 +9,40 @@ import { UserService } from 'src/app/user/user.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent {
-  constructor(private userService: UserService, private router: Router) { }
-  edit(form: NgForm): void {
-    // this.userService.edit();
-    this.router.navigate(['/']);
+  destination: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private destinationService: DestinationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.destination = params;
+    });
   }
-}
+
+  edit(form: NgForm): void {
+    const formData = form.value;
+    const title = formData.title;
+    const location = formData.location
+    const date = formData.date;
+    const image = formData.image;
+    const description = formData.description;
+
+    const data = {
+      title: title,
+      location: location,
+      date: date,
+      description: description,
+      image: image,
+    };
+
+    this.destinationService
+      .edit(this.destination._id, data)
+      .subscribe((res) => {
+        this.router.navigate([`destinations/${this.destination._id}`]);
+      });
+  }
+  }
