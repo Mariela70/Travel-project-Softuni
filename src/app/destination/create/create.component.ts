210c5766df3c9aa16b3520354f8bcc617c072fb4
Mariela <marielamiteva97@abv.bg>
2023-08-03 18:01:55 +0300

    added create destination functionality

--- src/app/destination/create/create.component.ts
+++ src/app/destination/create/create.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from 'src/app/user/user.service';
+import { DestinationService } from '../destination.service';
 
 @Component({
   selector: 'app-create',
@@ -9,9 +9,33 @@ import { UserService } from 'src/app/user/user.service';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(
+    private destinationService: DestinationService,
+    private router: Router
+  ) {}
+
   create(form: NgForm): void {
-    // this.userService.create();
-    this.router.navigate(['/']);
+    const formData = form.value;
+    const destination = formData.destination;
+    const imageUrl = formData.imageUrl;
+    const description = formData.description;
+    const type = formData.type;
+
+    const data = {
+      destination: destination,
+      description: description,
+      imageUrl: imageUrl,
+      type: type,
+    };
+
+    this.destinationService.create(data).subscribe(
+      (data) => {
+        this.router.navigate([`/destinations/${data._id}`]);
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
   }
-}
+  }
+
